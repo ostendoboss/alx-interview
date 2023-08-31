ea0690d2dde1d9aa4c500021b99997a66938109a
John Alawode <110995795+ostendoboss@users.noreply.github.com>
2023-08-31 21:31:45 +0100

    initial commit on N queens. with base case

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,86 +1,90 @@
 #!/usr/bin/python3
-"""
-The N queens puzzle is the challenge of placing N non-attacking queens
-on an N×N chessboard. Write a program that solves the N queens problem.
-
-Usage: nqueens N
-If the user called the program with the wrong number of arguments,
-print Usage: nqueens N, followed by a new line, and exit with the status 1
-where N must be an integer greater or equal to 4
-If N is not an integer, print N must be a number,
-followed by a new line, and exit with the status 1
-If N is smaller than 4, print N must be at least 4,
-followed by a new line, and exit with the status 1
-The program should print every possible solution to the problem
-One solution per line
-Format: see example
-You don’t have to print the solutions in a specific order
-You are only allowed to import the sys module
+"""N queens on a chess board without killing each other?
 """
 import sys
 
 
-def is_safe(board, row, col, N):
-    """Check for queens in the same column"""
-    for i in range(row):
-        if board[i][col] == 1:
-            return False
+def main(N):
+    """Main fucntion returning the combinations
+    """
+    combs = []
+    for i in range(N):
+        for j in range(N):
+            combs.append([i, j])
+    valids = []
+    current = []
+    invalids = []
+    j = 1
+    trials = 0
+    while j < N-1:  # start [0,1], then [0,2], etc ..
+        valid = [[0, j]]
+        i = 0
+        rounds = 0
+        while len(valid) < N and i < len(combs) and rounds < N * 2:
+            curr = combs[i]
+            validity = True
+            for queen in valid:
+                # Not mirror image
+                cond1 = not(queen[1] == curr[0] and queen[0] == curr[1])
+                # Not on same diagonal
+                cond2 = not(abs(queen[1] - curr[1]) == abs(queen[0] - curr[0]))
+                # Not on main diagonal
+                cond3 = not((queen[1] == queen[0]))
+                cond3 = True
+                # Not on same row with any element in valid
+                cond4 = not (queen[1] == curr[1])
+                # Not on same column as any element in valid
+                cond5 = not (queen[0] == curr[0])
+                # adhoc (*if [0,1] starts no even [2,0], [4,0], but [3,0] ,etc
+                cond6 = not(queen[1] == 0 and (queen[0] % 2) == (j % 2))
+                cond6 = True
+                if cond1 and cond2 and cond3 and cond4 and cond5 and cond6:
+                    pass
+                else:
+                    validity = False
+            if validity:
+                valid.append(curr)
+            i = i + 1
+            if i == len(combs) and rounds < N*2:
+                i = 0
+                rounds += 1
+        if len(valid) == N:
+            valids.append(valid)
+            j = j + 1
+        # reinitiliaze initials (invalids tracking would replace backtracking?)
+            invalids = []
+        else:
+            if valid:
+                value = valid.pop()
+                if not(value in invalids):
+                    invalids.append(value)
+            trials += 1
 
-    """Check for queens in the upper-left diagonal"""
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j] == 1:
-            return False
+            if trials == 3:
+                trials = 0
+                j += 1
+            print("i, j ", i, j)
+            print("invalids ", invalids)
+            print("valids ", valids)
 
-    """Check for queens in the upper-right diagonal"""
-    for i, j in zip(range(row, -1, -1), range(col, N)):
-        if board[i][j] == 1:
-            return False
-
-    return True
-
-
-def solve_nqueens(board, row, N, solutions):
-    """solve nqueens"""
-    if row == N:
-        solution = [[i, j] for i in range(N)
-                    for j in range(N) if board[i][j] == 1]
-        solutions.append(solution)
-        return
-
-    for col in range(N):
-        if is_safe(board, row, col, N):
-            board[row][col] = 1
-            solve_nqueens(board, row + 1, N, solutions)
-            board[row][col] = 0
-
-
-def print_solutions(solutions):
-    """print solutions"""
-    for solution in solutions:
-        print(solution)
-
-
-def main():
-    """main func"""
-    if len(sys.argv) != 2:
-        print("Usage: nqueens N")
-        sys.exit(1)
-
-    try:
-        N = int(sys.argv[1])
-    except ValueError:
-        print("N must be a number")
-        sys.exit(1)
-
-    if N < 4:
-        print("N must be at least 4")
-        sys.exit(1)
-
-    board = [[0 for _ in range(N)] for _ in range(N)]
-    solutions = []
-    solve_nqueens(board, 0, N, solutions)
-    print_solutions(solutions)
+        valid = []
+    return valids
 
 
 if __name__ == "__main__":
-    main()
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    try:
+        N = int(sys.argv[1])
+        if N >= 4:
+            queenset = main(N)
+            if queenset:
+                for a in queenset:
+                    print(a)
+        else:
+            print("N must be at least 4")
+            sys.exit(1)
+    except ValueError:
+        print("N must be a number")
+        sys.exit(1)
